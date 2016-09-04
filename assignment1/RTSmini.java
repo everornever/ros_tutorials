a25dc758bf71c09ad889197c5b2184c274be64ac
xiong <zhengxionguta@gmail.com>
2016-09-03 22:03:47 -0700

    comment on RTSmini.java

--- assignment1/RTSmini.java
+++ assignment1/RTSmini.java
@@ -13,8 +13,7 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
-public class RTSmini{
-	
+public class RTSmini{	
 	public static void main(String args[]) throws Exception{
 		for(int u=2;u<args.length;u++){
 			HashMap<String,Long> hm1=new HashMap<String,Long>();
@@ -40,7 +39,11 @@ public class RTSmini{
 			writeToFile(hm1,file);
 		}
 	}
-	
+	/**
+	 *this function takes the path of class as input and 
+	 *and analyse this specific class, return all of its 
+	 *methods stored in a MethodNode List.
+	*/	
 	public static List<MethodNode> getMethods(String classPath) throws Exception{
 		if(!new File(classPath).exists()){
 			return null;
@@ -53,7 +56,12 @@ public class RTSmini{
 		List<MethodNode> ls=cn.methods;
 		return ls;
 	}
-
+	/**
+	 *This function takes a HashMap, MethodNode and the path
+	 *of the class as inputs, read every instruction of the 
+	 *method and if it is another method then find its owner 
+	 *and analyseits owner class recursively.
+	 */  
 	public static  void readMethodInstructions(HashMap<String,Long> hm, MethodNode m,String filePath) throws Exception{
 		int t;
 		for(int j=0;j<m.instructions.size();j++){
@@ -79,6 +87,10 @@ public class RTSmini{
 		return;
 	}
 
+	/**
+	 *This function will return the hashcode of a method 
+	 *using Adler32 algorithm
+	 */
 	public static long getHash(MethodNode m){
 		int k;
 		byte[] opcode=new byte[m.instructions.size()];
@@ -92,14 +104,21 @@ public class RTSmini{
 		long hash=c.getValue();
 		return hash;
 	}
-	
+
+	/*This function is designed to compare two HashMaps so 
+	 *as to determine whether there is any change in the new 
+	 *programs
+	 */	
 	public static boolean findChanges(HashMap<String,Long> hm1,HashMap<String,Long> hm2){
 		for(String s1: hm1.keySet())
 			if(hm1.get(s1).compareTo(hm2.get(s1))!=0)
 				return false;
 		return true;
 	}
-	
+	/**
+	 *This function writes the dependencies stored in hashmap
+	 * to a file
+	 */ 	
 	public static void writeToFile(HashMap<String,Long> hm,String filename) throws IOException{
 	File f=new File(filename);	
 	FileOutputStream fos=new FileOutputStream(f);
