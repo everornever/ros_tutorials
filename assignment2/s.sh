ef6542f17d9830567a2b498420f0c7ed46e0802d
xiong <zhengxionguta@gmail.com>
2016-09-10 19:48:31 -0700

    modify s.sh

--- assignment2/s.sh
+++ assignment2/s.sh
@@ -3,5 +3,6 @@ jarFile="$(find ${1} -name "*.jar")"
 args1="$(find ${1} -name INPUT.java)"
 args2="$(find ${1} -name m.sh)"
 args3="${1}/MINI.java"
+touch $args3
 java -jar $jarFile $args1 $args2 $args3
 
