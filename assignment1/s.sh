16436e8bc5685605ca77306c4a8ffe0e1f714f81
xiong <zhengxionguta@gmail.com>
2016-09-10 22:04:54 -0700

    modify s.sh

--- assignment1/s.sh
+++ assignment1/s.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-dir="${1}"
-javac -classpath $dir "${3}.java"
-javac -classpath $dir "${4}.java"
 p="$(find -name RTSmini.java)"
 javac ${p}
 class1="$(find ${1} -name "*.java")"
@@ -11,5 +8,5 @@ javac ${class2}
 foo=$(find -name RTSmini.class)
 foo=${foo#$"./"}
 foo=${foo%$".class"}
-java $foo ${1} ${2} ${3} ${4}
+java $foo "$@"
 
