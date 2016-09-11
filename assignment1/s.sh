999226e554e2ef498a13f7a90e71a746941874a8
xiong <zhengxionguta@gmail.com>
2016-09-11 08:39:04 -0700

    modify s.sh to solve path problem

--- assignment1/s.sh
+++ assignment1/s.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+args=("$@")
+for ((i=2;i<$#;i++));do
+x[$(($i-2))]="$(find ${1} -name "${args[i]}.java")"
+x[$(($i-2))]=${x[$(($i-2))]%$".java"}
+done
+echo ${x[1]}
+echo ${x[@]}
 p="$(find -name RTSmini.java)"
 javac ${p}
 class1="$(find ${1} -name "*.java")"
@@ -8,5 +15,5 @@ javac ${class2}
 foo=$(find -name RTSmini.class)
 foo=${foo#$"./"}
 foo=${foo%$".class"}
-java $foo "$@"
+java $foo "${x[@]}"
 
