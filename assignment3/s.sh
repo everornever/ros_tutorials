2da6060151183140a5ffeadebc108ca7be25ac0f
xiong.zheng <zhengxionguta@gmail.com>
2016-09-18 12:33:33 -0400

    git clone repository of specific date

--- assignment3/s.sh
+++ assignment3/s.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 git clone ${1} "./javaDir"
+cd "./javaDir"
+commit="$(git rev-list --branches -n 1 --first-parent --before=2016-09-13)"
+git reset --hard $commit
+cd ".."
 path="$(find -name "javaDir")"
 mkdir -p "dir"
 p="$(find "./javaDir" -name "*.java")"
 cp $p "dir" 2>/dev/null
 jarFile="$(find -name "*.jar")"
-echo $jarFile
 java -jar $jarFile "dir"
